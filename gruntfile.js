3d09e7a634ab9d74be1cff7deb7bde63fa49ad53
bdadam <bdadam@gmail.com>
2016-03-15 03:15:35 +0100

    added watch task

--- gruntfile.js
+++ gruntfile.js
@@ -21,11 +21,23 @@ module.exports = function(grunt) {
                     to: '<%= grunt.file.read("src/loadfont.js") %>'
                 }]
             }
+        },
+
+        watch: {
+            source: {
+                files: ['src/**/*'],
+                tasks: ['default'],
+                options: {
+                    spawn: false,
+                }
+            }
         }
     });
 
     grunt.registerTask('default', ['uglify', 'replace']);
+    grunt.registerTask('dev', ['default', 'watch']);
 
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-text-replace');
 };
